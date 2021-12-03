c4ea0aa61b66e882560f35c9970780c8691f10dc
shalini jhajharia <shalinijhajharia185@gmail.com>
2021-12-03 12:42:07 +0530

    prograd 5665

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React  from 'react'
+class App1 extends React.Component
+{
+    render()
+    {
+        return <p>let's Rock  and Roll</p>
+    }
+}
+export default App1 ;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App1 from './App';
+function App()
+{
+    return <div>Hey ProGrads! Welcome to React Learning</div>
+}
+
+ReactDOM.render(<div><App/><App1/></div>,document.getElementById('root'));
+  
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
